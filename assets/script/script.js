eb65e2888637101616737fe5e93ad71382ce3d7a
Daniel Droppa <danwdroppa@gmail.com>
2020-02-07 16:48:19 -0500

    js layout for saving text content to local storage per hour

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,18 @@
-var saveBtn = $("#saveBtn[i]")
-
-$(saveBtn).on("click",function(){
-    
+// var saveBtn = $("#saveBtn");
+// var textHour = $("#texthour9");
+// var i = 9
+// $(saveBtn[i]).on("click",function(){
+// console.log(textHour[i].textcontent)
+// })
+$(".saveBtn").click(function(){
+    const value =$(this).val()
+    localStorage.setItem("hour"+value, $("#textArea"+value).val())
 })
+
+
+for (let i = 9; i < 17; i++) {
+    const toDo =localStorage.getItem("hour"+i)
+    if(toDo){
+        $("#textArea"+i).text(toDo)
+    }
+}
