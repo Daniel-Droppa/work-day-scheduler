974a733975aa75d25c295489b540a20e82091c67
Daniel Droppa <danwdroppa@gmail.com>
2020-02-07 22:54:49 -0500

    added JS to make past present and future color change take effect based on current time

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,24 @@
 $("#currentDay").html(Date(Date.now()));
-$(".saveBtn").click(function(){
-    const value =$(this).val()
-    localStorage.setItem("hour"+value, $("#textArea"+value).val())
+$(".saveBtn").click(function () {
+    const value = $(this).val()
+    localStorage.setItem("hour" + value, $("#textArea" + value).val())
     $("#currentDay").html(Date(Date.now()));
 })
 for (let i = 9; i < 17; i++) {
-    const toDo =localStorage.getItem("hour"+i)
-    if(toDo){
-        $("#textArea"+i).text(toDo)
+    const toDo = localStorage.getItem("hour" + i)
+    if (toDo) {
+        $("#textArea" + i).text(toDo)
     }
+}
+var currentHour =  new Date().getHours();
+$("#textArea" + currentHour).removeClass();
+$("#textArea" + currentHour).addClass("col-8 present");
+for (var t = 9; t < currentHour; t++) {
+    $("#textArea" + t).removeClass();
+    $("#textArea" + t).addClass("col-8 past");
 }
 
 
-var currentHour = new Date().getHours();
+console.log($("#textArea" + currentHour))
+
 console.log(currentHour);
