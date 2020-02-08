ebd8cd582b9a9b779594707be50b5f1ea3fea9fa
Daniel Droppa <danwdroppa@gmail.com>
2020-02-07 22:04:16 -0500

    finished HTML for rest of work day (hours 10-17)

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,18 +1,16 @@
-// var saveBtn = $("#saveBtn");
-// var textHour = $("#texthour9");
-// var i = 9
-// $(saveBtn[i]).on("click",function(){
-// console.log(textHour[i].textcontent)
-// })
+$("#currentDay").html(Date(Date.now()));
 $(".saveBtn").click(function(){
     const value =$(this).val()
     localStorage.setItem("hour"+value, $("#textArea"+value).val())
+    $("#currentDay").html(Date(Date.now()));
 })
-
-
 for (let i = 9; i < 17; i++) {
     const toDo =localStorage.getItem("hour"+i)
     if(toDo){
         $("#textArea"+i).text(toDo)
     }
 }
+
+
+var currentHour = new Date().getHours();
+console.log(currentHour);
